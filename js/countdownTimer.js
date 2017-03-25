a4dd7046903a4cf9aa211480267f904263fb72ca
Artur <tuifm@hotmail.com>
2017-03-25 15:37:23 -0300

    Link to JS timer

--- js/countdownTimer.js
+++ js/countdownTimer.js
@@ -1,5 +1,6 @@
 // link: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdown
 // http://stackoverflow.com/questions/1197928/how-to-add-30-minutes-to-a-javascript-date-object
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_setinterval_clearinterval !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // Get the target time
 var finishTime = new Date();
